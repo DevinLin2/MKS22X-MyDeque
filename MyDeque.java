4b079a85b851753a6e5be4b50abf49904c4894e2
Devin Lin <Dlin01@stuy.edu>
2019-03-27 10:26:16 -0400

    rewriting mydeque as the logic behind having start and end represent the space before the value caused some issues with loops

--- MyDeque.java
+++ MyDeque.java
@@ -1,28 +1,30 @@
+import java.util.*;
+
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
   public static void main(String[] args) {
     MyDeque data = new MyDeque();
-    data.addFirst("hi");
-    data.addFirst("there");
-    data.addFirst("bob");
-    data.addFirst("my");
-    data.addFirst("name");
-    data.addFirst("is");
-    data.addFirst("steve");
-    data.addFirst("whats");
-    data.addFirst("up");
-    data.addFirst("sack");
-    //data.removeFirst();
-    data.addFirst("bobby");
-    System.out.println(data.getLast());
-    System.out.println(data.getFirst());
-    System.out.println(data.size());
-    data.debug();
-    System.out.println(data.start);
-    System.out.println(data.end);
-    System.out.println(data);
+    // data.addFirst("hi");
+    // data.addFirst("there");
+    // data.addFirst("bob");
+    // data.addFirst("my");
+    // data.addFirst("name");
+    // data.addFirst("is");
+    // data.addFirst("steve");
+    // data.addFirst("whats");
+    // data.addFirst("up");
+    // data.addFirst("sack");
+    // //data.removeFirst();
+    // data.addFirst("bobby");
+    // System.out.println(data.getLast());
+    // System.out.println(data.getFirst());
+    // System.out.println(data.size());
+    // data.debug();
+    // System.out.println(data.start);
+    // System.out.println(data.end);
+    // System.out.println(data);
   }
 
   @SuppressWarnings("unchecked")
@@ -30,7 +32,7 @@ public class MyDeque<E>{
     data = (E[])new Object[10];
     size = 0;
     start = 0;
-    end = -1;
+    end = 0;
   }
 
   @SuppressWarnings("unchecked")
@@ -38,7 +40,7 @@ public class MyDeque<E>{
     data = (E[])new Object[initialCapacity];
     size = 0;
     start = 0;
-    end = -1;
+    end = 0;
   }
 
   public int size() {
@@ -47,15 +49,11 @@ public class MyDeque<E>{
 
   public String toString() {
     String ans = "{";
-    for (int i = start; i <= end; i++) {
-      if (i < 0) {
-        ans += data[i+data.length];
-      } else {
-        ans += data[i];
+    for (int i = start; i != end; i++) {
+      if (i > data.length - 1) {
+        i = 0;
       }
-      if (start < end) {
-        ans += " ";
-      }
+      ans += data[i] + " ";
     }
     return ans + "}";
   }
@@ -70,84 +68,38 @@ public class MyDeque<E>{
     System.out.println(ans);
   }
 
-  @SuppressWarnings("unchecked")
   public void addFirst(E element) {
-    if (start - 1 < 0) {
-      if (start * -1 == data.length - 1 || size == data.length) {
-        resize();
-      }
-      start--;
-      data[data.length+start] = element;
-      size++;
-    } else {
-      start--;
-      data[start] = element;
-      size++;
+    if (element == null) {
+      throw new NullPointerException("Enter a new valid element");
     }
   }
-
-  @SuppressWarnings("unchecked")
-  public void addLast(E element) {
-    if (size == data.length) {
-      resize();
-      start = 0;
-      end = size;
-    }
-    if (end == data.length - 1) {
-      end = 0;
-      data[end] = element;
-      size++;
-    } else {
-      if (size != 0) {
-        end++;
-      }
-      data[end] = element;
-      size++;
-    }
-  }
-  public E removeFirst() {
-    E removed = data[start];
-    data[start] = null;
-    start++;
-    size--;
-    if (start >= data.length) {
-      start = 0;
-    }
-    return removed;
-  }
-  public E removeLast() {
-    E removed = data[end];
-    data[end] = null;
-    end--;
-    size--;
-    if (end < 0) {
-      end = data.length - 1;
-    }
-    return removed;
-  }
-  public E getFirst() {
-    if (start < 0) {
-      return data[start + data.length];
-    }
-    return data[start];
-  }
-  public E getLast() {
-    if (end < 0){
-      return data[end + data.length];
-    }
-    return data[end];
-  }
-
-  @SuppressWarnings("unchecked")
-  private void resize() {
-    E[] newData = (E[]) new Object[size * 2 + 1];
-    for (int i = start; i <= end; i++) {
-      if (i < 0) {
-        newData[newData.length + i] = data[data.length + i];
-      } else {
-        newData[i] = data[i];
-      }
-    }
-    data = newData;
-  }
+  // public void addLast(E element) {
+  //   if (element == null) {
+  //     throw new NullPointerException("Enter a new valid element");
+  //   }
+  // }
+  // public E removeFirst() {
+  //   if (size == 0) {
+  //     throw new NoSuchElementException("The list is empty");
+  //   }
+  // }
+  // public E removeLast() {
+  //   if (size == 0) {
+  //     throw new NoSuchElementException("The list is empty");
+  //   }
+  // }
+  // public E getFirst() {
+  //   if (size == 0) {
+  //     throw new NoSuchElementException("The list is empty");
+  //   }
+  // }
+  // public E getLast() {
+  //   if (size == 0) {
+  //     throw new NoSuchElementException("The list is empty");
+  //   }
+  // }
+  //
+  // @SuppressWarnings("unchecked")
+  // private void resize() {
+  // }
 }
