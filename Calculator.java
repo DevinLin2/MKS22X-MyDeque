8bf4328830ff2ef3d40e7e9c3f028e2a95c696f3
Devin Lin <Dlin01@stuy.edu>
2019-03-27 21:05:57 -0400

    finished and tested

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Calculator{
   public static void main(String[] args) {
-    eval("8 2 + 99 9 - * 2 + 9 -");
+    System.out.println(eval("8 2 + 99 9 % * 2 + 9 -"));
   }
   /*Evaluate a postfix expression stored in s.
   *Assume valid postfix notation, separated by spaces.
@@ -10,14 +10,36 @@ public class Calculator{
   public static double eval(String s) {
     MyDeque data = new MyDeque();
     String[] values = s.split(" ");
-    double ans = 0.0;
     for (int i = 0; i < values.length; i++) {
       try {
-
+        double num = Double.parseDouble(values[i]);
+        data.addLast(num);
       } catch(NumberFormatException e) {
-        
+        double second = (double) data.removeLast();
+        double first = (double) data.removeLast();
+        double partialAns = 0.0;
+        if (values[i].equals("+")) {
+          partialAns = first + second;
+          data.addLast(partialAns);
+        }
+        if (values[i].equals("-")) {
+          partialAns = first - second;
+          data.addLast(partialAns);
+        }
+        if (values[i].equals("*")) {
+          partialAns = first * second;
+          data.addLast(partialAns);
+        }
+        if (values[i].equals("/")) {
+          partialAns = first / second;
+          data.addLast(partialAns);
+        }
+        if (values[i].equals("%")) {
+          partialAns = first % second;
+          data.addLast(partialAns);
+        }
       }
     }
-    return ans;
+    return (double) data.getLast();
   }
 }
